e6d89288c0b9cf8c1a0a3c8e6977518ce88ea7dd
Yevhen Demian <bukozdem@gmail.com>
2023-09-26 20:08:58 +0300

    added books entity, service and repo

--- src/main/java/book/online/BookApplication.java
+++ src/main/java/book/online/BookApplication.java
@@ -1,13 +1,34 @@
 package book.online;
 
+import static java.math.BigDecimal.valueOf;
+
+import book.online.model.Book;
+import book.online.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookApplication {
+    @Autowired
+    private BookService bookService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book testBook = new Book("Title", "Author", "0000",
+                    valueOf(100), "Description", "cover image");
+            Book anotherBook = new Book("Book", "Writer", "1111",
+                    valueOf(300), "Some text", "photo");
+            bookService.save(testBook);
+            bookService.save(anotherBook);
+            bookService.findAll();
+        };
+    }
 }
---
+++ src/main/java/book/online/model/Book.java
@@ -0,0 +1,39 @@
+package book.online.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String author;
+    @Column(nullable = false, unique = true)
+    private String isbn;
+    @Column(nullable = false)
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+
+    public Book(String title, String author, String isbn,
+                BigDecimal price, String description, String coverImage) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.price = price;
+        this.description = description;
+        this.coverImage = coverImage;
+    }
+}
